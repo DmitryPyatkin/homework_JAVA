fdc48ed2365d1f4c977860abd81db19aa36189dd
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2023-06-12 21:55:05 +0700

    Урок 1. Знакомство с языком программирования Java

---
+++ task_2.java
@@ -0,0 +1,30 @@
+// 2) Вывести все простые числа от 1 до 1000
+
+package homework1;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class task_2 {
+  public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите число:");
+    int number = scanner.nextInt();
+    scanner.close();
+    System.out.println("Простые числа до числа " + number + ":");
+    for (int i = 2; i <= number; i++) {
+      boolean isPrimeNumber = true;
+      for (int j = 2; j < i; j++) {
+        if (i % j == 0) {
+          isPrimeNumber = false;
+          break;
+        }
+      }
+
+      if (isPrimeNumber) {
+        System.out.print(i + ", ");
+      }
+    }
+    System.out.println();
+  }
+}
