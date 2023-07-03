bd36ae5872611b8726ecd32ba8e20784de86b3fd
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2023-07-03 22:00:03 +0700

    Урок 6. Хранение и обработка данных ч3: множество коллекций Set

---
+++ Menu.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Menu {
+  List<Notebook> laptops;
+
+  public Menu(List<Notebook> laptops) {
+    this.laptops = laptops;
+  }
+
+  public void showLaptops(List<Notebook> laptops) {
+    System.out.println("----------------------------------------------");
+    System.out.println("Model\t\t\tManufacturer\tPrice");
+    System.out.println("----------------------------------------------");
+    for (Notebook laptop : laptops) {
+      System.out.println(laptop);
+    }
+    System.out.println("----------------------------------------------");
+    // System.out.println("\n");
+  }
+
+  public void sortByAscendingPrice(List<Notebook> laptops) {
+    Collections.sort(laptops, new Comparator<Notebook>() {
+      @Override
+      public int compare(Notebook notebook1, Notebook notebook2) {
+        return Double.compare(notebook1.getPrice(), notebook2.getPrice());
+      }
+    });
+  }
+
+  public void sortByDescendingPrice(List<Notebook> laptops) {
+    Collections.sort(laptops, new Comparator<Notebook>() {
+      @Override
+      public int compare(Notebook notebook1, Notebook notebook2) {
+        return Double.compare(notebook2.getPrice(), notebook1.getPrice());
+      }
+    });
+  }
+
+  public void sortByAscendingManufacturer(List<Notebook> notebooks) {
+    Collections.sort(notebooks, new Comparator<Notebook>() {
+      @Override
+      public int compare(Notebook notebook1, Notebook notebook2) {
+        return notebook1.getManufacturer().compareTo(notebook2.getManufacturer());
+      }
+    });
+  }
+}
